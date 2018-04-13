a685a4ca848c7ba00043d6d07f070f42e8495939
Greg Ochab <greg@ochab.us>
2018-04-12 21:46:19 -0500

    Remove dead code, refine classes

--- script.js
+++ script.js
@@ -1,30 +1,32 @@
 class Hero {
-  constructor(health, shotsPerSecond, ammo, damagePerShot) {
+  constructor(name, health, shotsPerSecond, ammo, damagePerShot, reloadTime) {
+    this.name = name;
     this.health = health;
     this.shotsPerSecond = shotsPerSecond;
     this.ammo = ammo;
     this.damagePerShot = damagePerShot;
+    this.reloadTime = reloadTime; // milliseconds
   }
 }
 
 let millisecond = 0;
-let Tracer = new Hero (150, 20, 40, 6);
-let Soldier76 = new Hero (200, 9, 9.5, 25);
+let Tracer = new Hero ('Tracer', 150, 20, 40, 6, 1250);
+let Soldier76 = new Hero ('Solider:76', 200, 9, 9.5, 25, 1500);
 
 while(Tracer.health >= 1 && Soldier76.health >= 1) {
 
-  if(millisecond % 50 === 0) {
+  if(millisecond % (1000/Tracer.shotsPerSecond) === 0) {
     Tracer.ammo--;
     Soldier76.health -= Tracer.damagePerShot;
-    console.log(`Tracer Ammo: ${Tracer.ammo}, Soldier:76 Health: ${Soldier76.health}`);
+    console.log(`${Tracer.name} Ammo: ${Tracer.ammo}, ${Soldier76.name} Health: ${Soldier76.health}`);
   } else {
     // console.log('x');
   }
 
-  if(millisecond % 111 === 0) {
+  if(millisecond % (1000/Soldier76.shotsPerSecond) === 0) {
     Soldier76.ammo--;
     Tracer.health -= Soldier76.damagePerShot;
-    console.log(`Soldier:76 Ammo: ${Soldier76.ammo}, Tracer Health: ${Tracer.health}`);
+    console.log(`${Soldier76.name} Ammo: ${Soldier76.ammo}, ${Tracer.name} Health: ${Tracer.health}`);
   } else {
     // console.log('x');
   }
