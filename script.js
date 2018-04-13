938e3a6ba5706fdf5eae781fb3331b609fee6dbe
Greg Ochab <greg@ochab.us>
2018-04-12 22:24:44 -0500

    cleanup issues

--- script.js
+++ script.js
@@ -10,15 +10,15 @@ class Hero {
 }
 
 let millisecond = 0;
-let Tracer = new Hero ('Tracer', 150, 20, 40, 6, 1250);
-let Soldier76 = new Hero ('Soldier:76', 200, 9, 9.5, 25, 1500);
+let Tracer = new Hero ('Tracer', 150, 20, 20, 12, 1250);
+let Soldier76 = new Hero ('Soldier:76', 200, 8.66, 9.5, 19, 1500);
 
 function fight(redHero, blueHero) {
   while(redHero.health >= 1 && blueHero.health >= 1) {
 
     if(millisecond % (1000/redHero.shotsPerSecond) === 0) {
       redHero.ammo--;
-      blueHero.health -= blueHero.damagePerShot;
+      blueHero.health -= redHero.damagePerShot;
       console.log(`${redHero.name} Ammo: ${redHero.ammo}, ${blueHero.name} Health: ${blueHero.health}`);
     } else {
       // console.log('x');
@@ -33,7 +33,7 @@ function fight(redHero, blueHero) {
     }
 
     console.log(`— ${millisecond}ms —`);
-    millisecond = millisecond + 1;
+    millisecond++;
   }
 }
 
