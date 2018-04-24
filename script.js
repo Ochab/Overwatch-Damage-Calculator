929c314c16b8797d1b48ed90c341009380e3cd45
Greg Ochab <greg@ochab.us>
2018-04-24 18:55:25 -0500

    Added other attack heroes

--- script.js
+++ script.js
@@ -10,8 +10,15 @@ class Hero {
 }
 
 let millisecond = 0;
-let Tracer = new Hero ('Tracer', 150, 20, 20, 12, 1250);
+// Hero                   name, health, shotsPerSecond, ammo, damagePerShot, reloadTime
+let Genji     = new Hero ('Genji', 200, 1, 8, 84, 1500);
+let McCree    = new Hero ('McCree', 200, 2, 6, 70, 1500);
+let Pharah    = new Hero ('Pharah', 200, 1.1, 6, 120, 1000);
+let Reaper    = new Hero ('Reaper', 250, 2, 8, 140, 1500);
 let Soldier76 = new Hero ('Soldier:76', 200, 8.66, 9.5, 19, 1500);
+let Sombra    = new Hero ('Sombra', 200, 20, 60, 8, 1500);
+let Tracer    = new Hero ('Tracer', 150, 20, 20, 12, 1250);
+
 
 function fight(redHero, blueHero) {
   while(redHero.health >= 1 && blueHero.health >= 1) {
